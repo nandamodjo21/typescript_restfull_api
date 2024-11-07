c162d66a40d2222bb44a0550921661e5dd081dfc
Jeff4Dev <jeffmdev4@gmail.com>
2024-11-07 16:56:16 +0800

    commit

--- main.ts
+++ main.ts
@@ -1,14 +1,25 @@
-import express from "express";
-import publicRoute from "./src/routes/public_route";
+import express, {Request, Response} from "express";
+
 import { errorMiddleware } from "./src/middleware/error_middleware";
-import { startBot } from "./src/utils/star_sock";
 const app = express();
 import init from "./src/config/init";
 import { ResponseError } from "./src/error/response_error";
+import {UserController} from "./src/controller/user_controller";
+import {LoginController} from "./src/controller/login_controller";
+import {RegisterController} from "./src/controller/register_controller";
+import {ActivationController} from "./src/controller/activation_controller";
+import {OtpController} from "./src/controller/otp_controller";
+import publicRoute from "./src/routes/public_route";
 app.use(express.json());
-app.use(publicRoute);
 app.use(errorMiddleware);
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("halo");
+});
+app.get("/users", UserController.users);
+app.post("/login", LoginController.login);
+app.post("/register", RegisterController.register);
+app.get("/activation", ActivationController.activate);
+app.get("/otp",OtpController.activated);
 async function runInit() {
   try {
     await init();
@@ -16,7 +27,11 @@ async function runInit() {
     throw new ResponseError(404, `${e}`);
   }
 }
-runInit();
+runInit().then(r =>{
+  console.log("run init");
+} ).catch(reason => {
+  throw new ResponseError(500,`${reason}`);
+});
 
 app.listen(process.env.APP_PORT, function () {
   console.log(
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,7 @@ const access: ConnectionOptions = {
   user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
+  password:process.env.DB_PASSWORD,
 };
 
 const conn = mysql.createConnection(access);
